7de208f2bd47bee5070a8fa4b230ff6c9545d654
watchertv <wang983532chen787@163.com>
2024-12-14 18:32:42 +0800

    优化目录结构

--- bootstrap/uploader.js
+++ bootstrap/uploader.js
@@ -1,114 +1,56 @@
+/**
+ * 上传器
+ * @param files
+ * @param options
+ */
 import {http} from "./http";
 
-/**
- * 默认构造器
- * @param {[]} [files]
- */
-function Uploader(files) {
-	(files || []).forEach(path => {
-		this.push({
-			state: 0,
-			path: path
-		});
-	});
-	return this;
-}
-
-//继承数组原型链
-Uploader.prototype = [];
-
-//重构构造器
-Uploader.prototype.constructor = Uploader;
-
-/**
- * 上传
- * @param {number} index
- */
-Uploader.prototype.upload = function(index) {
-	const triggerChange = (file, index) => {
-		this.onUploadChange && this.onUploadChange.call(this, {
-			file: file,
-			index: index
-		});
-	};
-
-	const upload = (file, index) => {
-		return request({
-			url: 'upload/picture',
-			filePath: file.path,
-			name: 'file'
-		}).then((res) => {
-			file.state = 1;
-			file.url = res.data.picture_url;
-			file.picture_id = res.data.picture_id;
-			triggerChange(file, index);
-		}, () => {
-			file.state = 2;
-			triggerChange(file, index);
-		});
-	};
-
-	if (index !== undefined) { //单个上传
-		const item = this[index];
-		return upload(item, index);
-	} else { //全部上传，逐个上传
-		const sequentialUpload = (index) => {
-			upload(this[index], index).finally(() => {
-				if (index < this.length - 1) {
-					sequentialUpload(index + 1);
-				}
-			});
-		};
-		sequentialUpload(0);
+// 基础配置
+const config = (function() {
+	try {
+		let config = require('../config/upload.js');
+		if (typeof config === 'function') {
+			config = config();
+		}
+		return config || {};
+	} catch (e) {
+		console.warn(e);
+		return {};
 	}
-};
+})();
 
 /**
- * 添加上传文件
- * @param {string} path
- * @param {boolean} isAutoUpload
- * @return {number} index
+ * 上传器
+ * @param {Array} files
+ * @param {String} options
  */
-Uploader.prototype.add = function(path, isAutoUpload = true) {
-	const index = this.push({
-		state: 0,
-		path: path
-	}) - 1;
-	if (isAutoUpload) this.upload(index);
-	return index;
-};
+export default function uploader(files, options) {
+	const triggerChange = function(params) {
+		options.onUploadChange && options.onUploadChange.call(this, params);
+	};
 
-/**
- * 移除上传文件
- * @param {number} index
- */
-Uploader.prototype.remove = function(index) {
-	this.splice(index, 1);
-};
-
-/**
- * 获取已上传的文件
- */
-Uploader.prototype.getUploadedFiles = function() {
-	const result = [];
-	this.forEach((item) => {
-		if (item.url) result.push(item.url);
-	});
-	return result;
-};
-
-/**
- * 返回数组
- * @return {[]}
- */
-Uploader.prototype.toArray = function() {
-	return Array.from(this);
-};
-
-/**
- * 上传改变事件
- * @param {{file:*,index:number}} res
- */
-Uploader.prototype.onUploadChange = null;
-
-export default Uploader;
+	const upload = function(file, index) {
+		return http.request(Object.assign({
+			url: config.url
+		}, options, {
+			filePath: file
+		})).then(config.onUploadedSuccess || function(res) {
+			return {
+				url: res.data.picture_url,
+				picture_id: res.data.picture_id,
+			};
+		}).then((res) => {
+			res.state = 1;
+			res.index = index;
+			triggerChange(res);
+		}, (err) => {
+			err.state = 2;
+			triggerChange(err);
+		}).finally(() => {
+			if (index < files.length - 1) {
+				upload(index + 1);
+			}
+		});
+	};
+	upload(0);
+}
