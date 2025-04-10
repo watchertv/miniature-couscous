d4aa283e31289d4c51c228220ff745798524c8ce
watchertv <wang983532chen787@163.com>
2025-04-11 06:12:42 +0800

    修复优化

--- config/https/formid.request.interceptor.js
+++ config/https/formid.request.interceptor.js
@@ -1,14 +1,16 @@
-export default function(res) {
+export default function(config) {
 	// 上报formid
-	if (res.method === 'post') {
+	if (config.method === 'post') {
 		const formid = wx.pullFormid(20);
 		if (formid.length) {
-			res.data.__formid__ = formid;
+			config.data.__formid__ = formid;
 		}
 	} else {
 		const formid = wx.pullFormid();
 		if (formid.length) {
-			res.data.__formid__ = formid[0];
+			config.data.__formid__ = formid[0];
 		}
 	}
+
+	return config;
 };
