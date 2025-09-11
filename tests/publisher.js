70976413f14abbc931ef71638a81e0940211f876
watchertv <wang983532chen787@163.com>
2025-09-11 19:49:13 +0800

    uniapp版本

---
+++ tests/publisher.js
@@ -0,0 +1,15 @@
+const aaa = wx.publisher('aaa');
+aaa.add(function(event) {
+    console.log('sub1', event);
+}, function(event) {
+    console.log('sub2', event);
+}, function(event) {
+    console.log('sub3', event);
+}, function(event) {
+    console.log('sub4', event);
+}, function(event) {
+    console.log('sub5', event);
+});
+aaa.send({
+    user: '123'
+});
