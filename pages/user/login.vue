e0ce62f968f32223c8fac24a08f2c12d53a45f43
watchertv <wang983532chen787@163.com>
2026-02-24 22:12:14 +0800

    增加http自定义loading

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -14,6 +14,11 @@
 				<button form-type='submit'>登录</button>
 			</view>
 		</form>
+
+		<view :class="{show:isShowLoading}" class="loading">
+			<image src="/static/images/loading.gif" style="width: 100%" mode="widthFix"/>
+			Loading...
+		</view>
 	</div>
 </template>
 
@@ -21,9 +26,27 @@
 export default {
 	name: "login",
 	data() {
-		return {};
+		return {
+			isShowLoading: false,
+			showLoadingCount: 0
+		};
 	},
 	methods: {
+		showLoading() {
+			this.showLoadingCount++;
+			this.isShowLoading = true;
+		},
+
+		hideLoading() {
+			this.showLoadingCount--;
+			if (this.showLoadingCount <= 0) {
+				this.showLoadingCount = 0;
+				setTimeout(()=> {
+					this.isShowLoading = false;
+				},500);
+			}
+		},
+
 		/**
 		 * 登录
 		 */
@@ -31,12 +54,21 @@ export default {
 			const data = e.detail.value;
 			console.log(data);
 
-			uni.showLoading();
-			setTimeout(() => {
-				uni.navigateBack();
-				uni.$showTips('登录成功！');
-				// uni.$delayNavigateBack(1200);
-			}, 1000);
+			uni.$http.post('member/login', {
+				username: 'admin',
+				password: '123456'
+			}, {
+				loading: this
+			}).then(function() {
+				console.log(this)
+			});
+
+			// uni.showLoading();
+			// setTimeout(() => {
+			// 	uni.navigateBack();
+			// 	uni.$showTips('登录成功！');
+			// 	// uni.$delayNavigateBack(1200);
+			// }, 1000);
 		}
 	}
 }
@@ -45,5 +77,28 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 	.login {
+	}
+
+	.loading {
+		position: fixed;
+		/*background-color: rgba(0, 0, 0, 0.8);*/
+		background-color: #feeceb;
+		box-shadow: 0 0 15px #feeceb;
+		top: 15%;
+		left: 50%;
+		transform: translateX(-50%);
+		border-radius: 10upx;
+		z-index: -10;
+		color: white;
+		padding: 30upx;
+		width: 50%;
+		text-align: center;
+		transition: all .5s;
+		opacity: 0;
+	}
+
+	.loading.show{
+		opacity: 1;
+		z-index: 9999;
 	}
 </style>
